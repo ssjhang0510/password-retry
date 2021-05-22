f895260b9ecb2ecc8d4d8782176839376c8d104b
Aiden Jhang <ssjhang0510@gmail.com>
2021-05-22 19:39:59 +0800

    final version

--- password.py
+++ password.py
@@ -2,10 +2,12 @@
 password = 'a123456'
 x = 1
 while x <= 3:
+	x = x + 1
 	key = input('請輸入使用者密碼: ')
 	if key != password:
-		x = x + 1
-		print('密碼錯誤，還有',4-x,'次機會')
+		print('密碼錯誤')
+		if x < 4:
+			print('密碼錯誤，還有',4-x,'次機會')
 	else:
 		print('登入成功')
 		break
